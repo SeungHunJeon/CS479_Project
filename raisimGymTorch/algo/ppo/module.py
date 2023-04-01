feat: update Transformer encoding fix: Encoder gradient flow

raisimGymTorch/algo/ppo/module.py
@@ -3,7 +3,8 @@ import numpy as np
 import torch
 from torch.distributions import Normal
 from torch.distributions import Categorical
-from ..helper import Transformer
+from ..helper.Transformer import PositionalEncoding
+import math
 
 class Actor:
     def __init__(self, architecture, distribution, device='cpu'):
@@ -116,41 +117,66 @@ class Estimator:
     @property
     def obs_shape(self):
         return self.architecture.input_shape
+class Transformer(nn.Module):
+    def __init__(self, input_dim, hidden_dim, ext_dim, pro_dim, dyn_info_dim, dyn_predict_dim, act_dim, hist_num, batch_num, num_env, d_model, max_len, device):
+        super(Transformer, self).__init__()
+        self.ext_dim = ext_dim
+        self.pro_dim = pro_dim
+        self.act_dim = act_dim
+        self.dyn_info_dim = dyn_info_dim
+        self.dyn_predict_dim = dyn_predict_dim
+        self.hist_num = hist_num
+        self.device = device
+        self.hidden_dim = hidden_dim
+        self.num_env = num_env
+        self.batch_num = batch_num
+        self.input_dim = input_dim
+        # self.input_dim = input_dim*self.hist_num
+        self.block_dim = ext_dim + pro_dim + dyn_info_dim + act_dim
 
+        # Transformer
 
-class Transformer_Encoder(nn.Module):
-    def __init__(self, layer, N):
-        super(Transformer_Encoder).__init__()
-        self.layers = Transformer.clones(layer, N)
-        self.norm = Transformer.LayerNrom(layer.size)
+        self.embedding = nn.Linear(input_dim, d_model)
+        self.max_len = max_len
+        self.d_model = d_model
+        self.pe = PositionalEncoding(d_model=d_model,
+                                     max_len=max_len)
+        self.transformer_layer = nn.TransformerEncoderLayer(d_model=d_model,
+                                                            dim_feedforward=256,
+                                                            nhead=8)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer=self.transformer_layer,
+                                                 num_layers=4)
 
-    def forward(self, x, mask):
-        for layer in self.layers:
-            x = layer(x, mask)
-        return self.norm(x)
+        self.lin = nn.Linear(d_model, self.hidden_dim)
 
-# class ROA(nn.Module):
-#     def __init__(self, architecture, device='cpu'):
-#         super(ROA, self).__init__()
-#         self.architecture = architecture
-#         self.architecture.to(device)
-#
-#     def predict(self, obs):
-#         return self.architecture(obs).detach()
-#
-#     def evaluate(self, obs):
-#         return self.architecture(obs)
-#
-#     def evaluate_update(self, obs):
-#         return self.architecture.forward_update(obs)
-#
-#     def parameters(self):
-#         return [*self.architecture.parameters()]
-#
-#     @property
-#     def obs_shape(self):
-#         return self.architecture.input_shape
+        # self.init_weights(self.architecture, scale)
+        self.input_shape = [self.input_dim*batch_num]
+        self.output_shape = [hidden_dim]
+    def forward(self, obs):
+        inputs = obs.reshape(-1, self.num_env, self.input_dim)
+        inputs = self.embedding(inputs)
+        inputs = inputs * math.sqrt(self.d_model)
+        inputs = self.pe(inputs)
+        # print(outputs.shape)
+        outputs = self.transformer_encoder(inputs)
+        output = outputs[-1, :, :]
 
+        return output
+
+    def forward_update(self, obs):
+        inputs = obs.reshape(-1, self.num_env, self.input_dim)
+        inputs = inputs.reshape(self.max_len, -1, self.input_dim)
+        inputs = self.embedding(inputs)
+        inputs = inputs * math.sqrt(self.d_model)
+        inputs = self.pe(inputs)
+        # print(outputs.shape)
+        outputs = self.transformer_encoder(inputs)
+        outputs = outputs[-1, :, :]
+        output = outputs.reshape(-1, self.hidden_dim)
+
+        return output
+    def reset(self):
+        return True
 class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, ext_dim, pro_dim, dyn_info_dim, dyn_predict_dim, act_dim, hist_num, batch_num, num_env, layer_num, device):
         super(LSTM, self).__init__()
@@ -179,6 +205,8 @@ class LSTM(nn.Module):
         self.output_shape = [hidden_dim]
         self.h_0 = None
         self.c_0 = None
+
+    # Forward function is for encode one-step observation which incorporates number of (high-level controller frequency) / (low-level controller frequency)
     def forward(self, obs):
         inputs = obs.reshape(-1, self.num_env, self.input_dim)
 
@@ -195,6 +223,8 @@ class LSTM(nn.Module):
 
         return output
 
+    # Forward_update is for encode 1-iteration whole-step observation which incorporates number of
+    # (number of step) * (high-level controller frequency) / (low-level controller frequency)
     def forward_update(self, obs):
         inputs = obs.reshape((-1, self.num_env, self.input_dim))
 

raisimGymTorch/algo/ppo/ppo_LSTM.py
@@ -211,9 +211,7 @@ class PPO:
     def filter_for_latent_f_dynamics_from_obs(self, obs_batch, latent_batch_):
         latent_f_dynamics_input = []
 
-        latent_batch = latent_batch_.clone().detach()
-
-        latent_batch = latent_batch.reshape((-1, self.num_envs, self.encoder.architecture.hidden_dim))
+        latent_batch = latent_batch_.reshape((-1, self.num_envs, self.encoder.architecture.hidden_dim))
 
         latent_batch_input = latent_batch[:-1, ...]
 
@@ -420,6 +418,8 @@ class PPO:
             for obs_batch, actions_batch, old_sigma_batch, old_mu_batch, current_values_batch, advantages_batch, returns_batch, old_actions_log_prob_batch \
                     in self.batch_sampler(self.num_mini_batches):
 
+                if isinstance(self.encoder.architecture, nn.Transformer):
+                    print(1)
                 self.encoder.architecture.reset()
                 self.encoder_ROA.architecture.reset()
 
@@ -439,18 +439,18 @@ class PPO:
 
 
 
-                latent_f_dyn_input, latent_f_dyn_predict_true = self.filter_for_latent_f_dynamics_from_obs(obs_batch, latent)
+                latent_f_dyn_input, latent_f_dyn_predict_true = self.filter_for_latent_f_dynamics_from_obs(obs_batch, latent_d)
 
                 # decoder_input, decoder_output_true = self.filter_for_decoder_from_obs(obs_batch, latent)
                 #
                 # decoder_predict = self.decoder.evaluate(decoder_input)
 
-                obj_f_dyn_input, obj_f_dyn_true = self.filter_for_obj_f_dynamics_from_obs(obs_batch, latent)
+                obj_f_dyn_input, obj_f_dyn_true = self.filter_for_obj_f_dynamics_from_obs(obs_batch, latent_d)
 
                 # estimator_input = self.encoder_ROA.evaluate_update(obs_ROA_batch[-1, ...]).clone().detach().reshape(-1, self.encoder_ROA.architecture.hidden_dim)
 
-                estimator_input = self.encode(obs_batch[-1, ...]).reshape(-1, self.encoder.architecture.hidden_dim)
-
+                estimator_input = latent_d.reshape(-1, self.num_envs, self.encoder.architecture.hidden_dim)
+                estimator_input = estimator_input[-1, ...].squeeze(0)
                 estimator_loss = self.criteria(self.estimator.evaluate(estimator_input), estimator_true_data)
 
                 lambda_loss_ROA = self.lambda_ROA * self.criteria(latent, latent_ROA_d)
@@ -514,7 +514,6 @@ class PPO:
                        + obj_f_dynamics_loss \
                        + latent_f_dyn_loss \
                        + estimator_loss
-                       # + decoder_loss
 
                 # dynamics_loss = obj_f_dynamics_loss
 
@@ -529,7 +528,6 @@ class PPO:
                                           *self.critic.parameters(),
                                           *self.encoder.parameters(),
                                           *self.encoder_ROA.parameters(),
-                                          # *self.decoder.parameters()
                                           *self.estimator.parameters()
                                           ], self.max_grad_norm)
 

raisimGymTorch/env/envs/rsg_raibo_rough_terrain/runner_Transformer_dynamics.py
@@ -116,7 +116,8 @@ Estimator = ppo_module.Estimator(ppo_module.MLP(cfg['architecture']['estimator']
                                                 nn.LeakyReLU,
                                                 hidden_dim,
                                                 inertial_dim), device=device)
-Encoder_ROA = ppo_module.Encoder(architecture=ppo_module.LSTM(input_dim=int(ROA_Encoder_ob_dim/historyNum),
+
+Encoder_ROA = ppo_module.Encoder(architecture=ppo_module.Transformer(input_dim=int(ROA_Encoder_ob_dim/historyNum),
                                                           hidden_dim=hidden_dim,
                                                           ext_dim=ROA_ext_dim,
                                                           pro_dim=pro_dim,
@@ -126,10 +127,11 @@ Encoder_ROA = ppo_module.Encoder(architecture=ppo_module.LSTM(input_dim=int(ROA_
                                                           hist_num=historyNum,
                                                           device=device,
                                                           batch_num=batchNum,
-                                                          layer_num=layerNum,
-                                                          num_env=env.num_envs), device=device)
+                                                          num_env=env.num_envs,
+                                                          d_model=hidden_dim,
+                                                          max_len=historyNum), device=device)
 
-Encoder = ppo_module.Encoder(architecture=ppo_module.LSTM(input_dim=int(Encoder_ob_dim/historyNum),
+Encoder = ppo_module.Encoder(architecture=ppo_module.Transformer(input_dim=int(Encoder_ob_dim/historyNum),
                           hidden_dim=hidden_dim,
                           ext_dim=ext_dim,
                           pro_dim=pro_dim,
@@ -138,9 +140,10 @@ Encoder = ppo_module.Encoder(architecture=ppo_module.LSTM(input_dim=int(Encoder_
                           dyn_predict_dim=dynamics_predict_dim,
                           hist_num=historyNum,
                           batch_num=batchNum,
-                          layer_num=layerNum,
                           device=device,
-                          num_env=env.num_envs), device=device)
+                          num_env=env.num_envs,
+                          d_model=hidden_dim,
+                          max_len=historyNum), device=device)
 
 pytorch_total_params = sum(p.numel() for p in Encoder.architecture.parameters())
 
@@ -191,7 +194,7 @@ ppo = PPO.PPO(actor=actor,
 iteration_number = 0
 
 
-wandb.init(group="jsh",project=task_name,name=name)
+# wandb.init(group="jsh",project=task_name,name=name)
 
 if mode == 'retrain':
     iteration_number = load_param(weight_path, env, actor, critic, ppo.optimizer, saver.data_dir)
@@ -200,8 +203,6 @@ for update in range(iteration_number, 1000000):
     torch.cuda.empty_cache()
     start = time.time()
     env.reset()
-    Encoder.architecture.reset()
-    Encoder_ROA.architecture.reset()
     reward_ll_sum = 0
     done_sum = 0
     average_dones = 0.
@@ -249,8 +250,6 @@ for update in range(iteration_number, 1000000):
         # env.stop_video_recording()
         # env.turn_off_visualization()
         env.reset()
-        Encoder.architecture.reset()
-        Encoder_ROA.architecture.reset()
         env.save_scaling(saver.data_dir, str(update))
 
     data_log = {}
@@ -318,7 +317,7 @@ for update in range(iteration_number, 1000000):
 
     end = time.time()
 
-    wandb.log(data_log)
+    # wandb.log(data_log)
 
     print('----------------------------------------------------')
     print('{:>6}th iteration'.format(update))